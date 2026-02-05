d7a18fe6dc587c5d67def432823e55664b9ff1d4
aidan17715 <aidan.chia.tong@gmail.com>
2026-02-05 13:10:30 +0800

    Level 0

---
+++ src/main/java/John.java
@@ -0,0 +1,16 @@
+public class John {
+    public static void main(String[] args) {
+        String logo = "     _       _           \n"
+                + "    | | ___ | |__  _ __  \n"
+                + " _  | |/ _ \\| '_ \\| '_ \\ \n"
+                + "| |_| | (_) | | | | | | |\n"
+                + " \\___/ \\___/|_| |_|_| |_|\n";
+        
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm John");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
