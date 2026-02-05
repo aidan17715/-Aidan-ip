ff5656a4daaa67bba343764df9e178210dc17d25
aidan17715 <aidan.chia.tong@gmail.com>
2026-02-05 13:27:20 +0800

    Level 3

--- src/main/java/John.java
+++ src/main/java/John.java
@@ -8,7 +8,7 @@ public class John {
                 + "| |_| | (_) | | | | | | |\n"
                 + " \\___/ \\___/|_| |_|_| |_|\n";
         
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
         
         System.out.println("____________________________________________________________");
@@ -23,11 +23,22 @@ public class John {
             System.out.println("____________________________________________________________");
             
             if (input.equals("list")) {
+                System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    System.out.println(" " + (i + 1) + "." + tasks[i]);
                 }
+            } else if (input.startsWith("mark ")) {
+                int taskIndex = Integer.parseInt(input.substring(5)) - 1;
+                tasks[taskIndex].markAsDone();
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   " + tasks[taskIndex]);
+            } else if (input.startsWith("unmark ")) {
+                int taskIndex = Integer.parseInt(input.substring(7)) - 1;
+                tasks[taskIndex].markAsNotDone();
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println("   " + tasks[taskIndex]);
             } else {
-                tasks[taskCount] = input;
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println(" added: " + input);
             }
