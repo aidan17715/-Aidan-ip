92ee95d268bc620369f0b35633eb59f09c25de8d
aidan17715 <aidan.chia.tong@gmail.com>
2026-02-05 13:16:43 +0800

    Level 1

--- src/main/java/John.java
+++ src/main/java/John.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class John {
     public static void main(String[] args) {
         String logo = "     _       _           \n"
@@ -10,7 +12,21 @@ public class John {
         System.out.println(" Hello! I'm John");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
+        
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        
+        while (!input.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+            input = scanner.nextLine();
+        }
+        
+        System.out.println("____________________________________________________________");
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
+        
+        scanner.close();
     }
 }
