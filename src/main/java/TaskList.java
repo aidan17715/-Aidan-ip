1c8a3791829aba3a65e689325d0d9829b60420ae
aidan17715 <aidan.chia.tong@gmail.com>
2026-02-18 19:10:15 +0800

    Level 6: Add delete command and use ArrayList (A-Collections)

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,27 +1,29 @@
+import java.util.ArrayList;
+
 public class TaskList {
-    private static final int MAX_TASKS = 100;
-    private Task[] tasks;
-    private int taskCount;
+    private ArrayList<Task> tasks;
 
     public TaskList() {
-        this.tasks = new Task[MAX_TASKS];
-        this.taskCount = 0;
+        this.tasks = new ArrayList<>();
     }
 
     public void addTask(Task task) {
-        tasks[taskCount] = task;
-        taskCount++;
+        tasks.add(task);
     }
 
     public Task getTask(int index) {
-        return tasks[index];
+        return tasks.get(index);
+    }
+
+    public void deleteTask(int index) {
+        tasks.remove(index);
     }
 
     public int getTaskCount() {
-        return taskCount;
+        return tasks.size();
     }
 
-    public Task[] getTasks() {
+    public ArrayList<Task> getTasks() {
         return tasks;
     }
 }
--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -3,14 +3,14 @@ public class Ui {
     
     public void showWelcome() {
         System.out.println(LINE);
-        System.out.println(" Hello! I'm John");
+        System.out.println(" Good evening. I'm John");
         System.out.println(" What can I do for you?");
         System.out.println(LINE);
     }
 
     public void showGoodbye() {
         System.out.println(LINE);
-        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(" Thank you so much.");
         System.out.println(LINE);
     }
 
@@ -19,23 +19,27 @@ public class Ui {
     }
 
     public void showTaskAdded(Task task, int taskCount) {
-        System.out.println(" Got it. I've added this task:");
+        System.out.println(" Okay. I've added this task:");
         System.out.println("   " + task);
         System.out.println(" Now you have " + taskCount + " tasks in the list.");
     }
-
+    public void showTaskDeleted(Task task, int taskCount) {
+        System.out.println(" Noted. I've removed this task:");
+        System.out.println("   " + task);
+        System.out.println(" Now you have " + taskCount + " tasks in the list.");
+    }
     public void showTaskMarked(Task task) {
-        System.out.println(" Nice! I've marked this task as done:");
+        System.out.println(" Good. I've marked this task as done:");
         System.out.println("   " + task);
     }
 
     public void showTaskUnmarked(Task task) {
-        System.out.println(" OK, I've marked this task as not done yet:");
+        System.out.println(" Okay. I've marked this task as not done yet:");
         System.out.println("   " + task);
     }
 
     public void showTaskList(TaskList taskList) {
-        System.out.println(" Here are the tasks in your list:");
+        System.out.println(" Check this out. Here are the tasks in your list:");
         for (int i = 0; i < taskList.getTaskCount(); i++) {
             System.out.println(" " + (i + 1) + "." + taskList.getTask(i));
         }
